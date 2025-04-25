e03fb43c3d0047be6a38fe2eb380d4c8741b7728
Jpablo55 <135130507+Jpablo55@users.noreply.github.com>
2025-04-25 11:56:32 -0500

    Delete Python_Jhamile08 directory

--- Python_Jhamile08/ejercicio_1.py
+++
@@ -1,9 +0,0 @@
-# Calculadora de promedio con validación
-
-# Pide al usuario 3 notas (entre 0 y 10).
-
-#     Si alguna nota está fuera del rango, muestra un mensaje de error.
-#     Si todas son válidas, calcula el promedio y muestra si el estudiante aprueba (≥ 6) o reprueba.
-
-
-    
