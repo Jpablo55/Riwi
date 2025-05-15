58c2fcd467f65cd22f0238d4940dd4f9d2d3f202
Jpablo55 <135130507+Jpablo55@users.noreply.github.com>
2025-05-15 11:42:33 -0500

    Add files via upload

--- Python/Taller_Pedrosky/reserva_vuelos.py
+++ Python/Taller_Pedrosky/reserva_vuelos.py
@@ -10,11 +10,21 @@ gestion_vuelos:dict = {
 print(gestion_vuelos['AOK-029']['asientos disponibles'])
 def reserva_asientos(codigo_vuelo:str, numero_asiento:str):
     vuelo: dict = gestion_vuelos[codigo_vuelo]
-    asientos_disponibles = vuelo['asientos disponibles']
+    asientos_disponibles:dict = vuelo['asientos disponibles']
     if numero_asiento in asientos_disponibles:
         gestion_vuelos[codigo_vuelo]['asientos disponibles'].remove(numero_asiento)
         gestion_vuelos[codigo_vuelo]['asientos ocupados'].append(numero_asiento)
         print('\nSu reserva ha sido completada con éxito.\n')
         print(gestion_vuelos[codigo_vuelo]['asientos disponibles'])
         print(gestion_vuelos[codigo_vuelo]['asientos ocupados'])
+    else:
+        print('El asiento no está disponible')
 reserva_asientos('AOK-029', 'A2')
+
+def calculo_porcentaje_ocupado(codigo_vuelo):
+    cantidad_ocupados:int = len(gestion_vuelos[codigo_vuelo]['asientos ocupados'])
+    cantidad_disponibles:int = len(gestion_vuelos[codigo_vuelo]['asientos disponibles'])
+    total_asientos:int = cantidad_ocupados + cantidad_disponibles
+    porcentaje_ocupados:float = (cantidad_ocupados * 100)/total_asientos
+    print(f'Asientos ocupados: {porcentaje_ocupados}%')
+calculo_porcentaje_ocupado('AOK-029')
